206f7a00249473f9a3e8f35fed3b7c1309599234
Ashy <asd456ef@gmail.com>
2020-05-06 14:43:22 +0000

    fix chat to be normal

--- discord_bot.py
+++ discord_bot.py
@@ -4,45 +4,60 @@ import aiml
 import asyncio
 import os
 
+assert "TOKEN" in os.environ
+TOKEN = os.getenv("TOKEN")
+
+#  載入Alice核心
 os.chdir('alice')
 alice = aiml.Kernel()
 alice.learn("startup.xml")
 alice.respond('LOAD ALICE')
 
-TOKEN="Njk4NDIxMTM3MjY4Mjc3Mjc4.Xq51kw.qqEB-wImFdR2I5R7ODBDdj_QWc0"
-
 client = discord.Client()
-bot=commands.Bot(command_prefix="//") #設定//為指令
-
+bot = commands.Bot(command_prefix="//")  # 設定//為指令
 
 @bot.event
-async def on_ready() :
+async def on_ready():
+    """
+    確認機器人上線
+    """
     print("Bot is ready.")
-#確認機器人上線
+
 
 @bot.event
-async def on_member_join(member) :
+async def on_member_join(member):
+    """
+    在頻道發送加入伺服器訊息
+    """
     channel = bot.get_channel(698895507992346674)
     await channel.send(f'{member} join ! ')
-#在頻道發送加入伺服器訊息
+
+
 
 @bot.event
-async def on_member_remove(member) :
+async def on_member_remove(member):
+    """
+    在頻道發送離開伺服器訊息
+    """
     channel = bot.get_channel(698895507992346674)
     await channel.send(f'{member} leave ! ')
-#在頻道發送離開伺服器訊息
 
-#---------命令區-------------
+
+# ---------命令區-------------
+
 
 @bot.command()
-async def ping(ctx) :
+async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} ms')
 
-@bot.command()
-async def chat(ctx,ClientMessage) : 
-  RetMsg = alice.respond(ClientMessage)
-  await ctx.send(RetMsg)
-  
 
-bot.run(TOKEN) 
-#執行機器人
+@bot.command()
+async def chat(ctx, *args):
+    """
+    聊天功能
+    """
+    RetMsg = alice.respond(" ".join(args).lstrip())
+    await ctx.send(RetMsg)
+
+# 執行機器人
+bot.run(TOKEN)
